c5ee1930192d8a1439053747ec96ae54418a4a1e
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-05-21 12:44:00 -0400

    Bug Solve

--- ms-vehicles/src/main/java/com/autofix/msvehicles/repositories/vehiclesRepository.java
+++ ms-vehicles/src/main/java/com/autofix/msvehicles/repositories/vehiclesRepository.java
@@ -7,15 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface vehiclesRepository extends JpaRepository<vehiclesRepository, Long> {
+public interface vehiclesRepository extends JpaRepository<vehiclesEntity, Long> {
 
-    vehiclesRepository findByPatente(String patente);
-
-    public void deleteById(long id);
-
-    public vehiclesEntity save(vehiclesEntity vehicle);
-
-    public List<vehiclesRepository> findAll();
-
-    public String[] findAllPatente();
 }
