7a31e44d8f14f81b51e65d409d9ab229348c9d4e
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-05-21 13:32:55 -0400

    Ms1 Save Vehicle

--- ms-vehicles/src/main/java/com/autofix/msvehicles/controllers/vehiclesController.java
+++ ms-vehicles/src/main/java/com/autofix/msvehicles/controllers/vehiclesController.java
@@ -19,4 +19,28 @@ public class vehiclesController {
         vehiclesEntity vehicleNew = vehiclesService.saveVehicle(vehicle);
         return ResponseEntity.ok(vehicleNew);
     }
+
+    @GetMapping("/")
+    public ResponseEntity<Iterable<vehiclesEntity>> listVehicles() {
+        Iterable<vehiclesEntity> vehicles = vehiclesService.getVehicles();
+        return ResponseEntity.ok(vehicles);
+    }
+
+    @GetMapping("/{patente}")
+    public ResponseEntity<vehiclesEntity> getVehiclesByPatente(@PathVariable String patente){
+        vehiclesEntity vehicle = vehiclesService.getVehiclesByPatente(patente);
+        return ResponseEntity.ok(vehicle);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteVehicle(@PathVariable long id){
+        vehiclesService.deleteVehicle(id);
+        return ResponseEntity.ok("Vehiculo eliminado");
+    }
+
+    @GetMapping("/patentes")
+    public ResponseEntity<String[]> findAllPatente(){
+        String[] patentes = vehiclesService.findAllPatente();
+        return ResponseEntity.ok(patentes);
+    }
 }
--- ms-vehicles/src/main/java/com/autofix/msvehicles/services/vehiclesService.java
+++ ms-vehicles/src/main/java/com/autofix/msvehicles/services/vehiclesService.java
@@ -15,4 +15,24 @@ public class vehiclesService {
     public vehiclesEntity saveVehicle(vehiclesEntity vehicle){
         return vehiclesRepository.save(vehicle);
     }
+
+    //metodo para obtener todos los vehiculos
+    public Iterable<vehiclesEntity> getVehicles(){
+        return vehiclesRepository.findAll();
+    }
+
+    //metodo para obtener un vehiculo por patente
+    public vehiclesEntity getVehiclesByPatente(String patente){
+        return vehiclesRepository.findByPatente(patente);
+    }
+
+    //metodo para eliminar un vehiculo
+    public void deleteVehicle(long id){
+        vehiclesRepository.deleteById(id);
+    }
+
+    //metodo para obtener todas las patentes
+    public String[] findAllPatente(){
+        return vehiclesRepository.findAllPatente();
+    }
 }
