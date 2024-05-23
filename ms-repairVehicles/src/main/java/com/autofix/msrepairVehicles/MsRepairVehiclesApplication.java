7f41281ef61e893ac0127cad9653eae21b9d3c96
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-05-22 21:45:21 -0400

    Add Ms3

---
+++ ms-repairVehicles/src/main/java/com/autofix/msrepairVehicles/MsRepairVehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.autofix.msrepairVehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRepairVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRepairVehiclesApplication.class, args);
+	}
+
+}
