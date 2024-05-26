52bf674195cb8b99aabebcdb26d5621d4867279e
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-05-25 23:39:02 -0400

    Ms4 Update

--- ms-reports/src/main/java/com/autofix/ms_reports/clients/RepairVehiclesFeignClient.java
+++ ms-reports/src/main/java/com/autofix/ms_reports/clients/RepairVehiclesFeignClient.java
@@ -1,9 +1,18 @@
 package com.autofix.ms_reports.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient(name = "ms-repairVehicles", path = "/api/v1/repairVehicles")
-public class RepairVehiclesFeignClient {
+public interface RepairVehiclesFeignClient {
 
-    
+    @GetMapping("/Report1")
+    List<Object[]> getReport1();
+
+    @GetMapping("/Report2")
+    List<Object[]> getReport2(@RequestParam int year, @RequestParam int month);
+
 }
